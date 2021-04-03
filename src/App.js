2c6847a55f9042f9a3ad2b2242db2275037c7326
Philippe Roemer <philipperoemer@yahoo.com>
2021-04-03 11:17:44 -0400

    Dark/light toggle added

--- src/App.js
+++ src/App.js
@@ -3,14 +3,18 @@ import "./App.css";
 import TodoForm from "./components/TodoForm";
 
 function App() {
-    //first part of array is current state, second is the function that allows you to update the current state
-    //inputText (current state) : setInputText (update function)
     const [inputText, setInputText] = useState("");
     const [inputTime, setInputTime] = useState("");
     const [todoItems, setTodoItems] = useState([]);
+
+    const [darkMode, setDarkMode] = useState(false);
     return (
-        <div className="App">
-            <TodoForm inputText={inputText} setInputText={setInputText} inputTime={inputTime} setInputTime={setInputTime} todoItems={todoItems} setTodoItems={setTodoItems} />
+        <div className={darkMode ? "dark-mode" : "light-mode"}>
+            <div className="container">
+                <input type="checkbox" onChange={() => setDarkMode(!darkMode)} />
+                <h3>{darkMode ? "Dark" : "Light"} mode enabled</h3>
+                <TodoForm inputText={inputText} setInputText={setInputText} inputTime={inputTime} setInputTime={setInputTime} todoItems={todoItems} setTodoItems={setTodoItems} />
+            </div>
         </div>
     );
 }
