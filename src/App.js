bb086b594edccee1cd4aa411479d754f8454c7bc
Philippe Roemer <philipperoemer@yahoo.com>
2021-04-19 15:46:17 -0400

    modal test button added

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import React, { useState } from "react";
+import Modal from "react-modal";
 import "./App.css";
 import TodoForm from "./components/TodoForm";
 import TodoList from "./components/TodoList";
 
+Modal.setAppElement("#root");
 function App() {
     const [inputText, setInputText] = useState("");
     const [inputTime, setInputTime] = useState("");
     const [todoItems, setTodoItems] = useState([]);
 
     const [darkMode, setDarkMode] = useState(false);
+
+    const [modalIsOpen, setModalIsOpen] = useState(false);
     return (
         <div className={darkMode ? "dark-mode" : "light-mode"}>
             <div className="container">
@@ -18,6 +22,11 @@ function App() {
                 </div>
                 <TodoForm inputText={inputText} setInputText={setInputText} inputTime={inputTime} setInputTime={setInputTime} todoItems={todoItems} setTodoItems={setTodoItems} />
                 <TodoList todoItems={todoItems} setTodoItems={setTodoItems} />
+                <button onClick={() => setModalIsOpen(true)}>Open Modal</button>
+                <Modal isOpen={modalIsOpen} onRequestClose={() => setModalIsOpen(false)}>
+                    <p>Timer</p>
+                    <button onClick={() => setModalIsOpen(false)}>Close</button>
+                </Modal>
             </div>
         </div>
     );
